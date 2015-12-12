ac66bd5ff874e16ed66e9bcaa1ee0653b3493572
hadarigd <dan_hada_88@yahoo.com>
2015-12-12 12:33:13 +0200

    Create hobby.php

---
+++ views/hobby.php
@@ -0,0 +1,40 @@
+<main>
+    <div class="container">
+        <div class="row-main1">
+            <div id="fadein">
+                <img class="active" src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/poza-echipa.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/1.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/2.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/3.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/4.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/5.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/6.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/7.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/8.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/9.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/10.jpg">
+                <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/11.jpg">
+            </div>
+        </div>
+        <div class="row-main2">
+            <h2>O scurta introducere</h2>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+            </p>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+            </p>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+            </p>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+            </p>
+        </div>
+     </div>
+</main>
