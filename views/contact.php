3ffe36cbe0d2aacf091cec04607140c3905a7aec
hadarigd <dan_hada_88@yahoo.com>
2015-12-12 12:32:40 +0200

    Create contact.php

---
+++ views/contact.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <main class="formular">
+        <div class="contact">
+            <div id="er" align="center"></div>
+            <br/>
+            <form action="#" method="post">
+                <table id="tbl" align="center">
+                    <tr><td>Name:</td><td><input type="text" name="name" id="name"></td></tr>
+                    <tr><td>Last name:</td><td><input type="text" name="lname" id="lname"></td></tr>
+                    <tr><td>Email:</td><td><input type="text" name="mail" id="mail"></td></tr>
+                    <tr><td>Message:</td><td><textarea rows="10" cols="45" name="message" id="message"></textarea></td></tr>
+                    <tr><td></td><td><input type="submit" name="submit" id="submit" value="Send"></td></tr>
+                </table>
+             </form>
+        </div>
+    </main>
+</div>
