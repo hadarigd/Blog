b2f9e3c040eaa73fe4cbdfeec89de787b971b738
hadarigd <dan_hada_88@yahoo.com>
2015-12-12 12:33:38 +0200

    Create login.php

---
+++ views/login.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div align="center" class="formular-login">
+		<form method="POST" novalidate></br>
+			<span class="error">{{error}}</span>
+			</br>
+			</br>
+			<input type="email" name="email" placeholder="Email" id="mail"> 
+			<br/><br/>
+			<input type="password" name="password" placeholder="Password" id="password">   
+			<br/><br/>
+			<input type="submit" value="Login" id="submit">
+		</form>
+    </div>
+</div>
