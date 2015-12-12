2f992c6e297fb82989e8a2bed207cecbf5f10451
hadarigd <dan_hada_88@yahoo.com>
2015-12-12 12:34:08 +0200

    Create team.php

---
+++ views/team.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <main>
+        <div class="container-echipa">
+        	<h3  align="center">test test test test test test test </h3>
+            <div class="echipa">
+                <ol align="center">
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/dan%20H.jpg" alt="img" /><br/>Hadarig Dan</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/ilear/website/index.php"><span class="blur" class="item"><img  class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/ilea.jpg" alt="img" /><br/>Alin Ilea</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/ciosanp/Blog/MVC/blog/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_paul_ciosan.jpg" alt="img" /><br/>Ciosan Paul</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/mocans/MVC/part5/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_mocan.jpg" alt="img" /><br/>Mocan Daniel</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/hacicua/MVC/part4/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_hacichu.png" alt="img" /><br/>Hacicu Alex</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/lazurcaa/curs1/tema/index.html" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_x.jpg" alt="img" /><br/>Lazurca Andrei</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/fatig/blog/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_x.jpg" alt="img" /><br/>Fati Romeo</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/echertr/blog/part5/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_robert_echet.jpg" alt="img" /><br/>Echert Robert</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/hanganut/MVC/part5/proiect%20final/index.php?page=about" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_teodora.jpg" alt="img" /><br/>Hanganu Teodora</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/girdanr/MVC/Proiect_final/index.php?page=articles" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_girdan.jpg" alt="img" /><br/>Girdan Roxana</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/tanasea/test/MVC/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_tase.jpg" alt="img" /><br/>Tanase Andrei</span></a></li>
+                    <li class="colegi" calss="menu-item"><a href="http://188.166.119.187/workspace/simonv/final_project/index.php" target="_blank"><span class="blur" class="item"><img class="img" src="http://188.166.119.187/workspace/hadarigd/!Final%20prezentare/Blog%202016/public/img/imgColegi/rsz_dani_simon.jpg" alt="img" /><br/>Simon Daniel</span></a></li>
+                </ol>
+            </div>
+        </div>
+    </main>
+</div>
+
+
+
+
