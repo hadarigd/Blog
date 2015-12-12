ef36227c0d0ed7c49a453d9c50248e3cee0eedc9
hadarigd <dan_hada_88@yahoo.com>
2015-12-12 12:33:26 +0200

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,26 @@
+<main>
+    <div class="container">
+        <div class="row-main1">
+            <img id="poza" src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/poza%20munte.png"/>
+        </div>
+        <div class="row-main2">
+            <h2 align="center"> Pasiunea poate deveni o meserie</h2>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test "<a href="http://scoalainformala.ro/">Scoala informala de IT"</a> din Cluj Napoca.
+            </p>
+            	test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+            </p>
+            <p>
+                Daca doresti sa aplici pentu vreunul dintre cursurile organzate in cadrul scolii, te poti inscrie
+            	<a href="http://scoalainformala.ro/inscrieri/">aici</a>.
+            </p>
+        </div>
+     </div>
+</main>
