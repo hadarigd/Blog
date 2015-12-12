f3b5b599c6b7b342d95d3d6b640961dc809bca08
hadarigd <dan_hada_88@yahoo.com>
2015-12-12 12:31:29 +0200

    Create aboutme.php

---
+++ views/aboutme.php
@@ -0,0 +1,74 @@
+<main>
+    <div class="container">
+        <div class="row-main1">
+            <h1 align="center">Doua-trei cuvinte</h1>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+            </p>
+            <p>
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test 
+                test test test test test test test test test test test test test test test test test test test test test test
+            </p>
+        </div>
+        <div class="row-main2">
+            <img src="http://188.166.119.187/workspace/hadarigd/actualizare%20BLOG/public/img/poza-eu.jpg"/>
+            <h2>Poza</h2>
+            <p>...de la evenimentul de airsoft "Civil War 3" organiat de Ludus Legio</p>
+        </div>
+    </div>
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
